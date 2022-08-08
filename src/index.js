4e51cba0d9a0889db252cec12e02cfc1e90b0bd1
Akshay Patil <mailakshayspatil@gmail.com>
2022-08-08 17:03:14 +0530

    Fixed format mismatch in default filename

--- src/index.js
+++ src/index.js
@@ -6,8 +6,8 @@ const { cleanNodeProject, archiveProject } = require("./utils");
 
 const args = minimist(process.argv.slice(2));
 const projectName = args._;
-const outName = args.o || args.output || projectName + "-archived.zip";
 const format = args.f || args.format || "zip";
+const outName = args.o || args.output || `${projectName}-archived.${format}`;
 
 const projectPath = path.resolve(`./${projectName}`);
 const tempPath = path.resolve(`./${projectName}-temp`);
